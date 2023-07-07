77f05fb6e3133ad4645192d8bc9ba2e1c02907d0
Michael DesRoches <mdesroches@nevada.unr.edu>
2023-07-07 11:55:34 -0700

    css injections and prisma add

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,7 +1,27 @@
-import React from "react"
+import type { LinksFunction } from "@remix-run/node"
+import { Link } from "@remix-run/react"
 
-const App = () => {
-  return <h1>Hello Index Route</h1>
+import stylesUrl from "~/styles/index.css"
+
+export const links: LinksFunction = () => [
+  { rel: "stylesheet", href: stylesUrl },
+]
+
+export default function IndexRoute() {
+  return (
+    <div className="container">
+      <div className="content">
+        <h1>
+          Remix <span>Jokes!</span>
+        </h1>
+        <nav>
+          <ul>
+            <li>
+              <Link to="jokes">Read Jokes</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+  )
 }
-
-export default App
